307db63905185aec1851a7eb7896fea731d77efd
ZipLink ISP <ziplink@unxs.io>
2018-08-09 23:06:13 +0000

    Working on acct aggregation

--- Bitnami/AcctAggregation/aggregate.sh
+++ Bitnami/AcctAggregation/aggregate.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
-echo "testing 123";
+
+#We check for a known value that is only available after MySQL is completely available for use.
+
+#Required
+if [ "$cMysqlServer" == "" ];then
+	exit 1;
+fi
+if [ "$cMysqlLogin" == "" ];then
+	exit 2;
+fi
+if [ "$cMysqlPassword" == "" ];then
+	exit 3;
+fi
+
+cStatus="Fail";
+cReturn="";
+while [ $cStatus == "Fail" ]; do
+  cReturn=`/usr/bin/mysql -B -N -h $cMysqlServer -u$cMysqlLogin -p$cMysqlPassword radius -e 'SELECT "2"'|head -n 1`;
+  if [ "$?" == "0" ] && [ "$cReturn" == "2" ];then
+    cStatus="Ok";
+  else
+    echo "Waiting for MySQL";
+    sleep 10;
+  fi
+done
+
+#Aggregation Summary: 
+#	Here we write lock radacct, mysqldump radacct data, 
+#	if ok then we truncate radacct, release lock on radacct, Then we insert into master DB.
+#	Need to handle all failure scenarios, especially those regarding trunc radacct. Since
+#	that can cause data loss.
+for cIP in `/usr/bin/dig $cMysqlServer +short`;do
+	echo $cIP;
+done
 exit 0;
