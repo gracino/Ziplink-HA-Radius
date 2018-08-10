2eca15878ea498e8f05ee424106dd37a95ec91cf
Gary Wallis <ggw@unixservice.com>
2018-08-10 17:45:59 -0300

    Update delete.sh

--- Bitnami/AcctAggregation/delete.sh
+++ Bitnami/AcctAggregation/delete.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#Trun mysqldump into a DELETE FROM radacct sql file
+#Turn mysqldump into a DELETE FROM radacct sql file
 
 if [ "$1" == "" ];then
-	echo "usage: $0 InsertFileName";
+	echo "usage: $0 ReplaceFileName";
 	exit 0;
 fi
 
-for cAcctUniqueId in `grep "INSERT  IGNORE INTO" $1 | cut -f 3 -d ,`;do
+#for cAcctUniqueId in `grep "INSERT  IGNORE INTO" $1 | cut -f 3 -d ,`;do
+for cAcctUniqueId in `grep "REPLACE " $1 | cut -f 3 -d ,`;do
 	echo "DELETE FROM radacct WHERE acctuniqueid=$cAcctUniqueId;";
 done
