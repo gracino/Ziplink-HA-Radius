ff89428d6ca5fd3b2df8b74891c5faeac82c0fa0
Gary Wallis <ggw@unixservice.com>
2018-09-07 10:14:48 -0300

    Update clean-mysqld.log.sh

--- servers/clean-mysqld.log.sh
+++ servers/clean-mysqld.log.sh
@@ -4,5 +4,7 @@
 #	truncate all large container mysqld.log files.
 
 for cFile in `find /var/lib/docker/overlay2/ -name mysqld.log -size +300M`;do
+	#echo $cFile;
+        #break;
 	cat /dev/null > $cFile;
 done
